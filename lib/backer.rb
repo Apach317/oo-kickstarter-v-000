945c668b14313252b6effa55d4945fec214b6ade
Ron Lynch <lynch.ronald1@gmail.com>
2019-02-08 03:36:33 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -18,9 +18,11 @@
 #
 # - Similarly, whenever `add_backer()` is called to update `@backers`, the backer _should also update its `@backed_projects` list_.
 class Backer
-  attr_reader :name
+  attr_accessor :name
+  attr_reader :project
 
   def initialize
+    @name = name
     @backed_projects = []
     project = Project.new
   end
